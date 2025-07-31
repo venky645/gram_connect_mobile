e7080ce963d4d7707113725521bec4a47a9e40bd
Gopichand Bokkisam <gopichandbokkisam@gmail.com>
2025-07-31 19:01:04 +0530

    feat(login_view): update layout structure and styling

--- lib/presentation/auth/login_view.dart
+++ lib/presentation/auth/login_view.dart
@@ -8,8 +8,128 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  bool _obscureText = true;
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.deepPurple.shade800, Colors.purpleAccent.shade200],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Welcome',
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1,
+                ),
+              ),
+              SizedBox(height: 24),
+              Container(
+                padding: EdgeInsets.all(24),
+                margin: EdgeInsets.symmetric(horizontal: 32),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: Form(
+                  key: _formkey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please Enter Username";
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person_2_outlined),
+                          hintText: "Username",
+                          labelText: "Username",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      TextFormField(
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: _obscureText,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please Enter Password";
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.key_outlined,
+                            color: Colors.black.withOpacity(0.3),
+                          ),
+                          hintText: "Password",
+                          labelText: "Password",
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _obscureText = !_obscureText;
+                              });
+                            },
+                            icon: _obscureText
+                                ? Icon(Icons.visibility_outlined)
+                                : Icon(Icons.visibility_off_outlined),
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                        ),
+                      ),
+                      //SizedBox(height: 5),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text("Forgot Password?"),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _formkey.currentState!.validate();
+                          });
+                        },
+                        child: Text("Login"),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Don't have a Account?"),
+                          TextButton(onPressed: () {}, child: Text("Register")),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
